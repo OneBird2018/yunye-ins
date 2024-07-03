d73f39e6bcf289efc8703655a2b1130bbe846596
云烨 <147016162+OneBird2018@users.noreply.github.com>
2024-07-03 21:39:19 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://ddys.art/";//your website url
+  let target = "https://ddys.tv/";//your website url
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
